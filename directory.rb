85148e088b452229cf868625b80b4ce3dcddfb09
Jacques Laffy <jacques.laffy@gmail.com>
2018-11-23 15:29:46 +0000

    Centred text

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 #Student Array:
 def input_students
-  puts "Please enter the name, then height then nationality of each student"
-  puts "To finish, just hit return three times"
+  puts "Please enter the name, then height then nationality of each student".center(70)
+  puts "To finish, just hit return three times".center(70)
   students = []
   name = gets.chomp
   height = gets.chomp
@@ -18,21 +18,21 @@ end
 
 #Student List:
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(70)
+  puts "-------------".center(70)
 end
 
 def print(names)
   count = 0
   while count < names.length
-    puts "#{names[count][:name]} (#{names[count][:cohort]} cohort) [#{names[count][:nation]}: #{names[count][:height]}]"
+    puts "#{names[count][:name]} (#{names[count][:cohort]} cohort) #{names[count][:nation]}: #{names[count][:height]}".center(70)
     count += 1
   end
 end
 
 #Student Count:
 def print_footer(names)
-  puts "Overall we have #{names.count} great students"
+  puts "Overall we have #{names.count} great students".center(70)
 end
 
 #Method Calls:
