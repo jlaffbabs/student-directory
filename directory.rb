e1169295ad85e86937071ca8581cdc369af573ce
Jacques Laffy <jacques.laffy@gmail.com>
2018-11-25 19:47:24 +0000

    Refactored Interactive Menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
 #Student Array:
+@students = []
 def input_students
   puts "Please enter the name, then cohort, height and then nationality of each student".center(70)
   puts "To finish, just hit return four times".center(70)
-  students = []
   name = gets.chomp
   cohort = gets.tr("\r\n", "")
   height = gets.tr("\r\n", "")
@@ -11,62 +11,17 @@ def input_students
     cohort = "n/a" if cohort.empty?
     height = "n/a" if height.empty?
     nation = "n/a" if nation.empty?
-    students << {name: name, cohort: cohort, height: height, nation: nation}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort, height: height, nation: nation}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     name = gets.tr("\r\n", "")
     cohort = gets.tr("\r\n", "")
     height = gets.tr("\r\n", "")
     nation = gets.tr("\r\n", "")
   end
-  puts "Would you like to edit any students? Y/N"
-  edit = gets.tr("\r\n", "").upcase
-  while edit == "Y"
-    puts students
-    puts "Which student?"
-    stud_edit = gets.tr("\r\n", "")
-    students.each do |student|
-      if student[:name] == stud_edit
-        puts "What would you like to edit? (name/cohort/height/nation)"
-        cat_edit = gets.chomp
-        if cat_edit == "name"
-          puts "Enter your edit"
-          student[:name] = gets.tr("\r\n", "")
-        elsif cat_edit == "cohort"
-          puts "Enter your edit"
-          student[:cohort] = gets.tr("\r\n", "")
-        elsif cat_edit == "height"
-          puts "Enter your edit"
-          student[:height] = gets.tr("\r\n", "")
-        elsif cat_edit == "nation"
-          puts "Enter your edit"
-          student[:nation] == gets.tr("\r\n", "")
-        end
-      end
-    end
-    puts "Would you like to edit again? Y/N"
-    edit = gets.tr("\r\n", "").upcase
-  end
-  students
-end
-
-def cohort_sort(students)
-  sorted_by_cohort = {}
-  students.each do |student|
-    cohort = student[:cohort]
-    name = student[:name]
-    height = student[:height]
-    nation = student[:nation]
-
-    if sorted_by_cohort[cohort] == nil
-      sorted_by_cohort[cohort] = []
-    end
-    sorted_by_cohort[cohort].push("#{name}, Height: #{height}, Nationality: #{nation}")
-  end
-  sorted_by_cohort
 end
 
 #Student List:
@@ -75,43 +30,53 @@ def print_header
   puts "-------------".center(70)
 end
 
-def print(names)
-  if names.count == 0
+def print_students_list
+  if @students.count == 0
     puts "Villans Academy is currently empty".center(70)
   else
-    names.each do |cohort|
+    @students.each do |cohort|
       puts cohort
     end
   end
 end
 
 #Student Count:
-def print_footer(names)
-  if names.count > 0
-    puts "Overall we have #{names.count} great students".center(70)
+def print_footer
+  if @students.count > 0
+    puts "Overall we have #{@students.count} great students".center(70)
   end
 end
 
 #Interactive Menu:
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
