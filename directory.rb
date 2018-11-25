f7fbb00757c6b67549024baeadf36ce5b7396298
Jacques Laffy <jacques.laffy@gmail.com>
2018-11-25 19:33:12 +0000

    Interactive Menu

--- directory.rb
+++ directory.rb
@@ -92,9 +92,28 @@ def print_footer(names)
   end
 end
 
+#Interactive Menu:
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 #Method Calls:
-students = input_students
-coh = cohort_sort(students)
-print_header
-print(coh)
-print_footer(students)
+interactive_menu
